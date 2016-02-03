f86bcfa2f22e6d2298b2d085eb13fe35f9266cd1
WorkMac <backendservtestuser@backendservmac.bedford.progress.com>
2016-02-03 16:57:52 +0200

    Created initial project

---
+++ CookMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CookMe
+//
+//  Created by BackendServTestUser on 2/3/16.
+//  Copyright © 2016 MartinApostolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CookMe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CookMe
+//
+//  Created by BackendServTestUser on 2/3/16.
+//  Copyright © 2016 MartinApostolov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
